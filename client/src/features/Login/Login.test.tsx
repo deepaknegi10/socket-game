7296f028ec2d7e0a05ad9ba5a7f7f5c935413604
Deepak Negi <d.negi10.86@gmail.com>
2024-03-10 23:42:57 +0100

    More tests

--- client/src/features/Login/Login.test.tsx
+++ client/src/features/Login/Login.test.tsx
@@ -1,8 +1,17 @@
 import { render, screen } from "@testing-library/react"
 import { SocketServerMock } from "socket.io-mock-ts"
+import userEvent from "@testing-library/user-event"
 import { MemoryRouter } from "react-router-dom"
 import Login from "./Login"
 import roomData from "../../mocks/mockRoomData.json"
+
+const renderAppWithRouter = () => {
+  render(
+    <MemoryRouter>
+      <Login socket={client} roomData={roomData} loading={false} error={null} />
+    </MemoryRouter>
+  )
+}
 
 const socket = new SocketServerMock()
 const client = socket.clientMock
@@ -21,17 +30,27 @@ describe("App test", () => {
   })
 
   test("renders login page initially", () => {
-    render(
-      <MemoryRouter>
-        <Login
-          socket={client}
-          roomData={roomData}
-          loading={false}
-          error={null}
-        />
-      </MemoryRouter>
-    )
-
+    renderAppWithRouter()
     expect(screen.getByText("Login")).toBeInTheDocument()
+  })
+
+  test("renders the component with the correct label and placeholder", () => {
+    renderAppWithRouter()
+
+    const labelElement = screen.getByText("Username*")
+    expect(labelElement).toBeInTheDocument()
+
+    const inputElement = screen.getByPlaceholderText("Username")
+    expect(inputElement).toBeInTheDocument()
+  })
+
+  test("updates the state when the input value changes", () => {
+    renderAppWithRouter()
+
+    const inputElement = screen.getByPlaceholderText("Username")
+    userEvent.type(inputElement, "testuser")
+
+    // @ts-ignore
+    expect(inputElement.value).toBe("testuser")
   })
 })
